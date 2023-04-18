edda90cc61e62fdabc290751f1259e1ac81483e3
vinhvoit225 <98505412+vinhvoit225@users.noreply.github.com>
2023-04-18 15:46:44 +0700

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--share"
+export COMMANDLINE_ARGS="--share --skip-torch-cuda-test"
 
 # python3 executable
 #python_cmd="python3"
